fd17189a0df25b188b9ffe68add6cc3579a5fcb2
Isaac <957970078@qq.com>
2021-11-01 17:08:26 +0800

    第三次提交

--- plot_matrix.py
+++ plot_matrix.py
@@ -4,13 +4,11 @@ import imageio
 
 
 def generate_random_matrix(m, n):
-    random_matrix = np.random.rand(m,n)
-    return random_matrix 
-
+    random_matrix = np.random.randint(0, 2, (m, n))
+    return random_matrix
 
 def save_matrix(matrix, file_name):
-    imageio.imsave(file_name, matrix)
-    
+    imageio.imsave(file_name, matrix)   
 
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
