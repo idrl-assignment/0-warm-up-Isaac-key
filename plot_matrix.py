fea1c4ed42c6fa9a2fe1b23b4e9c7fba0fa15968
Isaac <957970078@qq.com>
2021-11-01 17:16:02 +0800

    第四次提交

--- plot_matrix.py
+++ plot_matrix.py
@@ -7,8 +7,10 @@ def generate_random_matrix(m, n):
     random_matrix = np.random.randint(0, 2, (m, n))
     return random_matrix
 
+
 def save_matrix(matrix, file_name):
-    imageio.imsave(file_name, matrix)   
+    imageio.imsave(file_name, matrix)
+    
 
 if __name__ == "__main__":
     matrix = generate_random_matrix(10, 10)
